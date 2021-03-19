4d62eb222c6fb3353d9f4b19140fbe43ed54483d
Niket <niketshukla@gmail.com>
2021-03-19 13:16:02 +0530

    Dropdown modified

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+
     const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+
         return(
             <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
                 {option.label}
@@ -13,10 +19,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">Select a color</label>
-                <div className="ui selection dropdown visible active">
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? "visible active" : ""}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open ? "visible transition" : ""}`}>
                         {renderedOptions}
                     </div>
                 </div>
